1656036ee2d6f49fe3eb75781f4f622640544384
kashyappatel5321 <131168070+kashyappatel5321@users.noreply.github.com>
2023-04-19 22:28:10 +0530

    Add files via upload

---
+++ 3areaoftriangle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+int base,height;
+float area;
+printf("height of triangle:");
+scanf("%d",&height);
+printf("base of triangle:");
+scanf("%d",&base);
+area=height*base*0.5;
+printf("area of triangle: %f",area);
+getch();
+return 0;
+}
+
+    
