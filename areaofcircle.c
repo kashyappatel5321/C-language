431d761f79d68930f5ab3c994e4204916544ab88
kashyappatel5321 <131168070+kashyappatel5321@users.noreply.github.com>
2023-04-19 22:29:14 +0530

    for find area of circle

---
+++ areaofcircle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    int r;
+    float a=3.14,b;
+    printf("pls enter radius:");
+    scanf("%d",&r);
+    printf("yuor circle's area is %f",b=a*r*r);
+    getch();
+    return 0;
+}
